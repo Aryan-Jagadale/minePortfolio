6dc4e50cdc9395e1ae7b28d53fc6eb09dcd91c8b
Aryan-Jagadale <aryansjagadale@gmail.com>
2022-12-31 16:45:46 +0530

    Doneeeeee

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Header from "./components/Header";
 import "./styles/home.scss";
 import MenuManager from "./components/Menu/MenuManager";
 import { Routes, Route } from "react-router-dom";
-import ToolKit from "./containers/ToolKit";
 import CustomCursor from "./components/CustomCursor";
-import 'react-toastify/dist/ReactToastify.css';
 import Contact from "./pages/ContactPage";
 import Intro from "./pages/IntroPage";
+
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
@@ -20,7 +20,6 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Intro />} />
-          <Route path="/toolkit" element={<ToolKit />} />
           <Route path="/collab" element={<Contact />} />
         </Routes>
       </MenuManager>
